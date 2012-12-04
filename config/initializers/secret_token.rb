cc5c8586cdffc4255ef924731ad3fbde756e34f4
Chris Hansen <moosekid5@gmail.com>
2012-12-04 15:17:37 -0800

    Remade the project to remove all devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Speedrunner::Application.config.secret_token = '11d185d1ab8f9ada5d6d03c0eb2de78c32fa1d45f8cab97c5af2c4f014c4be175f60362589d9d2f0219e4594b325de00d553bf90e6ce6182e0839906bc31d76f'
