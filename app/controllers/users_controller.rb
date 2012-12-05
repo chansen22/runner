3dfcabcf6b8ebf0f4296eeab2ed2f53066065ca3
Chris Hansen <moosekid5@gmail.com>
2012-12-04 20:54:40 -0800

    Got some validations working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, notice: "Welcome to SpeedRunner #{@user.first_name}"
+    else
+      render 'new', notice: "Something went wrong"
+    end
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ActiveRecord::Base
   has_many :moderates
   has_many :follows
 
-  attr_accessible :email, :first_name, :last_name, :password_digest, :username
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :username
   has_secure_password
 
-  
+  validates(:first_name, presence: true)
+  validates(:last_name, presence: true)
+  validates(:email, presence: true, format: { with: /@/i })
+  validates(:username, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false })
+  validates(:password, presence: true, length: { minimum: 3 }, confirmation: true)
+  validates(:password_confirmation, presence: true)
+
+  before_validation(on: :create) do |user|
+    user.username = user.username.downcase
+  end
 end
