338212243bff05d5a54a0e9ecd8f344f386b972f
Chris Hansen <moosekid5@gmail.com>
2012-12-05 14:29:37 -0800

    Updated the game form

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,11 @@
 class Game < ActiveRecord::Base
   attr_accessible :name, :publisher, :release_year, :series, :system
-	has_many :runs, :dependent => :destroy
-	has_many :moderates, :dependent => :destroy
-	has_many :users, :through => :moderates
+  has_many :runs, :dependent => :destroy
+  has_many :moderates, :dependent => :destroy
+  has_many :users, :through => :moderates
+
+  validates(:name, presence: true)
+  validates(:publisher, presence: true)
+  validates(:release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year } )
+  validates(:system, presence: true)
 end
