aaeccc7fb5b4889141c49298f6874c7d8b018610
Chris Hansen <moosekid5@gmail.com>
2012-12-07 18:58:07 -0800

    Made it so unverified runs dont show up on the games runs table

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,7 +3,9 @@ class Run < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
   has_many :comments, :dependent => :destroy
+
   default_scope order("time_in_seconds ASC")
+  
   validates(:time_in_seconds, presence: true, format: { with: /[0-9]{2}:[0-5][0-9]:[0-5][0-9]/ })
   validates(:category, presence: true)
   validates(:url, presence: true)
