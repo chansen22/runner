071a8b106786a70304b1c392b2dd7c2abef40fb2
Chris Hansen <moosekid5@gmail.com>
2012-12-07 18:43:41 -0800

    Added validations for runs for url and category and time, also added some regular expressions to check if those were of proper format and added a before save on the url to check if it has http:// or https:// at the front

--- app/models/run.rb
+++ app/models/run.rb
@@ -4,4 +4,13 @@ class Run < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
   default_scope order("time_in_seconds ASC")
+  validates(:time_in_seconds, presence: true, format: { with: /[0-9]{2}:[0-5][0-9]:[0-5][0-9]/ })
+  validates(:category, presence: true)
+  validates(:url, presence: true)
+
+  before_save do |run|
+    if run.url !~ /^(http:\/\/|https:\/\/).*\.com/
+      run.url = "http://#{run.url}"
+    end
+  end
 end
