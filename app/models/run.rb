9967d4db02aaa400364efa5ec283dedc1260da54
Chris Hansen <moosekid5@gmail.com>
2012-12-07 18:03:12 -0800

    Got it so every run is now verified by default and the url field requires you to add http

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,11 +1,6 @@
 class Run < ActiveRecord::Base
-  attr_accessible :category, :time_in_seconds, :url, :verified
+  attr_accessible :category, :time_in_seconds, :url
   belongs_to :game
   belongs_to :user
   has_many :comments, :dependent => :destroy
-
-  def self.set_correct_time(time)
-    times = time.split(":")
-    logger.info("\n\n\n\n\n\nTIME IS #{time} and times is #{times}")
-  end
 end
