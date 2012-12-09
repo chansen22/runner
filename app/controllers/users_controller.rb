39fa26743596bc5ebd01674bf74729de6cd74240
Chris Hansen <moosekid5@gmail.com>
2012-12-09 15:40:21 -0800

    Added runs to the user show page, closes #12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @runs = @user.runs.all
     if signed_in?
       @following = Follow.where("followee_id = ? AND user_id = ?", params[:id], current_user.id)
     end
